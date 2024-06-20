51facd7dd1b493e948bf21ce481873e188c275a1
Kingson.Arum <nwajiudo@gmail.com>
2024-06-20 14:23:07 +0100

    commit lottery.js file

---
+++ src/lottery.js
@@ -0,0 +1,48 @@
+import web3 from "./web3";
+
+const address = "0x2A82b5796198E8E367Bd33997A54fC8fd4e03c41";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: le,
+    type: "constructor",
+  },
+];
